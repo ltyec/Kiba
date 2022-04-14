6c325cac9acf7af7f58d8ab838d6f3d1e7fa2aef
ltyec <68455197+ltyec@users.noreply.github.com>
2022-04-15 01:24:11 +0800

    Update sth.py

--- src/plugins/sth.py
+++ src/plugins/sth.py
@@ -6,20 +6,27 @@ import requests
 from nonebot.adapters.cqhttp.message import MessageSegment
 import time
 
+msg1 = 0
+msg2 = 0
+msg3 = 0
+
 fudu = on_regex("[\s\S]*")
 
 @fudu.handle()
 async def first_msg(bot: Bot, event: Event, state: T_State):
+  global msg1
   msg1 = str(event.get_message())
   pass
 
 @fudu.got("next")
 async def next_msg(bot: Bot, event: Event, state: T_State):
+  global msg2
   msg2 = str(event.get_message())
   pass
 
 @fudu.got("final")
 async def final_msg(bot: Bot, event: Event, state: T_State):
+  global msg1, msg2, msg3
   msg3 = str(event.get_message())
   if msg1==msg2==msg3:
     await fudu.finish(msg)
