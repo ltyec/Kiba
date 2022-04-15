40a501be25fc04913086f58563d06b1f4651bb5f
ltyec <68455197+ltyec@users.noreply.github.com>
2022-04-15 17:13:08 +0800

    Update sth.py

--- src/plugins/sth.py
+++ src/plugins/sth.py
@@ -1,4 +1,4 @@
-from nonebot import on_regex, on_notice
+from nonebot import on_regex, on_keyword
 from nonebot.adapters.cqhttp import Message
 from nonebot.typing import T_State
 from nonebot.adapters import Bot,Event
@@ -9,7 +9,6 @@ import time
 msg1 = 0
 msg2 = 0
 msg3 = 0
-msg4 = 0
 
 fudu = on_regex("[\s\S]*")
 
@@ -33,18 +32,13 @@ async def final_msg(bot: Bot, event: Event, state: T_State):
   global msg2, msg3
   msg3 = str(event.get_message())
   if msg2 == msg3:
-    await fudu.send(msg1)
+    await fudu.finish(msg1)
     pass
   else:
     return
 
-while msg3:
-  @fudu.got("check")
-  async def check_msg(bot: Bot, event: Event, state: T_State):
-    global msg3, msg4
-    msg4 = str(event.get_message())
-  if msg3 == msg4:
-    time.sleep(1)
-    continue
-  else:
-    break
+blhx = on_keyword("granbluefantasy.jp")
+
+@blhx.handle()
+async def _(bot: Bot, event: Event, state: T_State):
+  await fudu.finish("好钓", at_sender=True)
