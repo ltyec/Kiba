7f397e1dcba71bbdaf94972a451e561ff9a7aac1
ltyec <68455197+ltyec@users.noreply.github.com>
2022-04-15 12:30:46 +0800

    Update sth.py

--- src/plugins/sth.py
+++ src/plugins/sth.py
@@ -9,6 +9,7 @@ import time
 msg1 = 0
 msg2 = 0
 msg3 = 0
+msg4 = 0
 
 fudu = on_regex("[\s\S]*")
 
@@ -20,15 +21,29 @@ async def first_msg(bot: Bot, event: Event, state: T_State):
 
 @fudu.got("next")
 async def next_msg(bot: Bot, event: Event, state: T_State):
-  global msg2
+  global msg1, msg2
   msg2 = str(event.get_message())
-  pass
+  if msg1 == msg2:
+    pass
+  else:
+    return
 
 @fudu.got("final")
 async def final_msg(bot: Bot, event: Event, state: T_State):
-  global msg1, msg2, msg3
+  global msg2, msg3
   msg3 = str(event.get_message())
-  if msg1==msg2==msg3:
-    await fudu.finish(msg1)
+  if msg2 == msg3:
+    await fudu.send(msg1)
+    pass
   else:
     return
+
+while msg3:
+  @fudu.got("check")
+  async def check_msg(bot: Bot, event: Event, state: T_State):
+    global msg3, msg4
+    msg4 = str(event.get_message())
+    if msg3 == msg4:
+      continue
+    else:
+      break
