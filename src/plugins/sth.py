dc94a20a8ed1f6ddcf509a043e260000fcaac3be
ltyec <68455197+ltyec@users.noreply.github.com>
2022-04-15 17:19:51 +0800

    Update sth.py

--- src/plugins/sth.py
+++ src/plugins/sth.py
@@ -41,4 +41,4 @@ blhx = on_keyword("granbluefantasy.jp")
 
 @blhx.handle()
 async def _(bot: Bot, event: Event, state: T_State):
-  await fudu.finish("好钓", at_sender=True)
+  await fudu.finish("骑空士钓你妈呢🥵", at_sender=True)
